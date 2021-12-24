8a0ae44c11c389228002a2604275b7705b297508
ratovoson81 <ratovoson81@gmail.com>
2021-12-25 02:05:58 +0300

    feat(data): picks

---
+++ data/Picks.js
@@ -0,0 +1,12 @@
+export const PicksData = [
+  {
+    title: "Bakery",
+    url: "https://firebasestorage.googleapis.com/v0/b/test-sayna-fe3c4.appspot.com/o/unsplash__79ZJS8pV70.png?alt=media&token=03d0b20c-fd16-416b-9a5c-f5573b4c8441",
+    id: 1,
+  },
+  {
+    title: "Fast Food",
+    url: "https://firebasestorage.googleapis.com/v0/b/test-sayna-fe3c4.appspot.com/o/unsplash_kcA-c3f_3FE.png?alt=media&token=5c8637cd-2ca3-4c33-a1e9-121f257f4bd6",
+    id: 2,
+  },
+];
