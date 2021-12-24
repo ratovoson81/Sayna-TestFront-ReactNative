8fc4132b9d989f8867f91dade7dcb314a0385946
ratovoson81 <ratovoson81@gmail.com>
2021-12-25 00:00:24 +0300

    feat(card): init caroussel card

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,49 @@
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import { RootTabScreenProps } from "../types";
+import tailwind from "tailwind-rn";
+import React from "react";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import Carousel from "../components/Caroussel";
+import { dummyData } from "../data/Data";
+//import Carousel, { Pagination } from "react-native-x2-carousel";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+const DATA = [{ text: "#1" }, { text: "#2" }, { text: "#3" }];
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  function renderItem(data: any) {
+    return (
+      <View key={data.text} style={styles.item}>
+        <Text>{data.text}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={tailwind("mt-8")}>
+      <View style={tailwind("flex flex-row justify-between")}>
+        <View style={tailwind("flex flex-row")}>
+          <View style={tailwind("ml-2")}>
+            <SimpleLineIcons name="location-pin" size={24} color="black" />
+          </View>
+          <View style={tailwind("ml-2")}>
+            <Text style={tailwind("text-lg")}>2600 Chadwick Rd</Text>
+            <Text style={tailwind("text-sm text-gray-400")}>
+              Mariette, GA 30066, USA
+            </Text>
+          </View>
+        </View>
+
+        <View style={tailwind("flex flex-row items-center")}>
+          <Text>O </Text>
+          <Text>Offers</Text>
+        </View>
+      </View>
+      <Text style={tailwind("text-xl mt-4 ml-2 font-bold")}>
+        Today'Featured
+      </Text>
+      <Carousel data={dummyData} />
     </View>
   );
 }
@@ -17,16 +51,15 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  item: {
+    width: 200,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#dbf3fa",
   },
 });
